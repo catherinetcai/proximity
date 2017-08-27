e93cd9d04a524582e5f280afd60c4e3cd38bce58
Cat Cai <catherinetcai@gmail.com>
2017-08-27 10:46:46 -0700

    Add in vendored. Start working on optimized query

--- proximity/proximity.go
+++ proximity/proximity.go
@@ -2,8 +2,10 @@ package proximity
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/golang/geo/s1"
 	"github.com/shopspring/decimal"
 )
 
@@ -36,4 +38,26 @@ func (d *DB) FindClosestHaversine(lat, lon decimal.Decimal) {
 		err = rows.StructScan(&l)
 		spew.Dump(l)
 	}
+}
+
+func (d *DB) FindClosestOptimized(lat, lon decimal.Decimal) {
+	distance := 10
+	// 1 degree of latitude is ~69 miles
+	// 1 degree of longitude ~ cos(latitude)*69
+	lon1 := lon - distance/math.Abs(math.Cos(s1.Angle(lat).Radians()*69))
+	lon2 := lon + distance/math.Abs(math.Cos(s1.Angle(lat).Radians()*69))
+	lat1 := lat - (distance / 69)
+	lat2 := lat + (distance / 69)
+	query := fmt.Sprintf(`
+		SELECT *, 3956 * 2 * ASIN(SQRT(
+			POWER(SIN((%v - locations.latitude) * pi()/180/2), 2) +
+			COS(%v * pi()/180) * COS(locations.latitude * pi()/180) *
+			POWER(SIN((%v - locations.longitude) * pi()/180/2), 2))) as distance
+		FROM locations
+		WHERE locations.longitude 
+		between %v and %v
+		and locations.latitude
+		between %v and %v
+		ORDER BY distance limit 20;
+	`, lat, lat, lon, lon1, lon2, lat1, lat2)
 }
